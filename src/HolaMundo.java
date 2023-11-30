7fe7f06dca27310f9c84e11a553595d1bd2f3bf9
fatimay14 <fyaalab01@educarex.es>
2023-11-30 14:13:33 +0100

    Otra vez

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -4,8 +4,9 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
-		System.out.println("Buenos días");
+		System.out.println("Buenos Días");
 		
+		System.out.println("Aquí de nuevo");
 		
 
 	}
